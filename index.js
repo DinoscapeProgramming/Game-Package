b1f85009dc19d37aa8f9c13e62c9cbf5b4a96157
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-05-22 17:14:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ module.exports = {
   users: require('./src/users.js'),
   matches: require('./src/matches.js'),
   leaderboard: require('./src/leaderboard.js'),
-  gameEngine: require('./src/gameEngine.js'),
-  games: fs.readdirSync(__dirname + '/src/games').reduce((data, item) => ({ ...data, [item.substring(0, item.length - 3)]: require("./src/games/" + item) }), {})
+  gameRegistry: require('./src/gameRegistry.js'),
+  util: require('./src/util.js'),
+  engine: require('./src/engine.js'),
+  games: fs.readdirSync(__dirname + '/src/games').reduce((data, item) => ({ ...data, [item.substring(0, item.length - 3)]: require("./src/games/" + item) }), {}),
+  templates: fs.readdirSync(__dirname + '/src/templates').reduce((data, item) => ({ ...data, [item.substring(0, item.length - 3)]: require("./src/templates/" + item) }), {}),
+  defaultOptions: require('./src/defaultOptions.json')
 }
